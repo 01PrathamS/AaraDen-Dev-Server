29520b486640daa2d04956f7a70d1da027dd5b54
Pratham Savaliya <pratham21btai35@karnavatiuniversity.edu.in>
2025-08-11 12:16:01 -0500

    code updated

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,6 +1,2 @@
 from src.routes.call_stats import stats_bp
-<<<<<<< HEAD
 from src.routes.auth import auth_bp
-=======
-from src.routes.auth import auth_bp
->>>>>>> 020d3ddfaee983344de078e66416422045e5255e
